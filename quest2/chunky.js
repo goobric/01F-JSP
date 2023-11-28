d79af125dc455d1e2cda65365904d27620a23c18
mlaane <goobric@gmail.com>
2023-11-28 16:48:49 +0100

    chunks of an array

---
+++ quest2/chunky.js
@@ -0,0 +1,11 @@
+function chunk(array, size) {
+  var chunks = [];
+  for (var i = 0; i < array.length; i += size) {
+    chunks.push(array.slice(i, i + size));
+  }
+  return chunks;
+}
+
+console.log(chunk([1, 2, 3, 4], 2)); // [[1, 2], [3, 4]]
+console.log(chunk([1, 2, 3, 4, 5], 2)); // [[1, 2], [3, 4], [5]]
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3)); // [[1, 2, 3], [4, 5, 6], [7, 8]]
