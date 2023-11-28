88e0670ca51bc0b41e71b7ff8e67f07e5751b949
mlaane <goobric@gmail.com>
2023-11-28 18:15:27 +0100

    path and src

---
+++ quest2/get.js
@@ -0,0 +1,20 @@
+function get(src, path) {
+  var keys = path.split(".");
+  var result = src;
+  for (var i = 0; i < keys.length; i++) {
+    result = result[keys[i]];
+  }
+  return result;
+}
+
+// var obj = {
+//   a: {
+//     b: {
+//       c: 1,
+//     },
+//   },
+// };
+
+const src = { nested: { key: "peekaboo" } };
+const path = "nested.key";
+get(src, path); // -> 'peekaboo'
