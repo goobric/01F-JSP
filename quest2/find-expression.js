597c5a8fcb68ea2e37a5ac6a5c0758f0887a9177
mlaane <goobric@gmail.com>
2023-11-28 19:47:19 +0100

    find expression2

--- quest2/find-expression.js
+++ quest2/find-expression.js
@@ -22,3 +22,15 @@ function findExpression(n) {
   }
   return undefined;
 }
+
+// The output of findExpression(10) would be 1*2*2+4.
+// Here's the sequence of operations:
+// Start with 1.
+// Multiply by 2 to get 2.
+// Multiply by 2 again to get 4.
+// Add 4 to get 8.
+// Add 4 again to get 12, which is greater than 10, so this path is not valid.
+// Go back to step 3, but this time add 4 after multiplying by 2 once. This gives 1*2+4 = 6, which is less than 10, so continue.
+// From 6, multiply by 2 to get 12, which is greater than 10, so this path is not valid.
+// Go back to step 6, but this time add 4 after getting 6. This gives 1*2+4+4 = 10, which is the target number.
+// So, the sequence of operations is 1*2+4+4, but the function findExpression always tries to multiply before adding, so it will return 1*2*2+4 instead.
