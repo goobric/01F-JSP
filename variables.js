b813bc95f3f2a492522a9e90e5064e111e3c90ff
mlaane <goobric@gmail.com>
2023-11-29 10:36:28 +0100

    variables in js

---
+++ variables.js
@@ -0,0 +1,15 @@
+// VARIABLES
+// Declered variables with var, let, const
+// var is global scope
+// let is block scope
+// const is block scope and constant
+// Assign variables before they are used, normally at the top of the scope
+
+// FORMATTING OF VARIABLE NAMES
+// firstName = camelCase
+// first_name = snake_case
+// FirstName = PascalCase
+// first-name = kebab-case
+// firstname = lowercase
+
+// Re-assigning variables
