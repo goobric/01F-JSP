cafb2679e08f2ac1af60b1c84fb5fa3439d3cfe5
mlaane <goobric@gmail.com>
2023-11-29 11:45:02 +0100

    its a match

---
+++ quest3/its-a-match.js
@@ -0,0 +1,9 @@
+var normal = /hi/;
+var begin = /^hi/;
+var end = /hi$/;
+var beginEnd = /^hi$/;
+
+// Path: quest3/its-a-match.js
+// now you can test the regular expressions directly in the console.
+// For example, you can type normal.test('hello hi') and press Enter to see if 'hi' is found in the string 'hello hi'.
+// The test method will return true if the string matches the regular expression, and false otherwise.
